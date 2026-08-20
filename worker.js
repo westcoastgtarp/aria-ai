7eb7ba44e6435022cb97f38d649e41542e93dcc0
westcoastgtarp <157748538+westcoastgtarp@users.noreply.github.com>
2026-08-20 15:18:59 -0700

    Fix Cloudflare PBKDF2 iteration limit

--- worker.js
+++ worker.js
@@ -42,7 +42,7 @@ async function sha256(value) {
 
 async function hashPassword(password) {
   const salt = crypto.getRandomValues(new Uint8Array(16));
-  const iterations = 210000;
+  const iterations = 100000;
   const key = await crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'PBKDF2', false, ['deriveBits']);
   const bits = await crypto.subtle.deriveBits({ name: 'PBKDF2', hash: 'SHA-256', salt, iterations }, key, 256);
   return `pbkdf2-sha256$${iterations}$${bytesToHex(salt)}$${bytesToHex(bits)}`;
