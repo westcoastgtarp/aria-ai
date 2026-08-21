c1fbe94fc13559d80faa96060f7cf2b66ab563bd
westcoastgtarp <157748538+westcoastgtarp@users.noreply.github.com>
2026-08-20 18:32:30 -0700

    Route member signup through Worker entrypoint

--- worker-entry.js
+++ worker-entry.js
@@ -1,10 +1,15 @@
 import baseWorker from './worker.js';
 import { handleStaffProvisioningRoute } from './staff-provisioning-api.js';
+import { handleMemberSignupRoute } from './member-signup-api.js';
 
 export default {
   async fetch(request, env, ctx) {
-    const response = await handleStaffProvisioningRoute(request, env);
-    if (response) return response;
+    const staffResponse = await handleStaffProvisioningRoute(request, env);
+    if (staffResponse) return staffResponse;
+
+    const memberSignupResponse = await handleMemberSignupRoute(request, env);
+    if (memberSignupResponse) return memberSignupResponse;
+
     return baseWorker.fetch(request, env, ctx);
   }
 };
